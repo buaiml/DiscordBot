64640cbc51ba3e9469cc548ecc0e0777b30cd088
Collin Barber <collinjbarber@gmail.com>
2024-09-12 00:42:01 -0400

    add join message

--- discordbot/main.py
+++ discordbot/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 import discord
 from discord import app_commands
@@ -45,8 +46,29 @@ class DiscordClient(discord.Client):
             print(f"An error occurred while syncing commands: {e}")
 
     async def on_member_join(self, member: discord.Member):
-        #member.send(content=content, embed=embed)
-        pass
+        # Waiting 10 seconds, so we don't ping right away.
+        await asyncio.sleep(10)
+
+        content = (f"Welcome {member.mention} to **AI Society**! We're happy to have you here.\n\n"
+                   f"We have a little notification system to help keep you up to date with events. "
+                   f"If you run the command `/notifications`, you can choose to be notified about events "
+                   f"24 hours, and 1 hour before they start (We recommend turning on both).")
+
+        next_event = None
+        events = await self.supabase_managers[Event].list()
+        for event in events:
+            if event.start_time > time.time() and event.status == "scheduled":
+                next_event = event
+                break
+
+        if next_event:
+            content += (f"\n\nIt looks like we have an [event coming up]({next_event.discord_link}). "
+                        f"Make sure to check it out! Use <#1228852708677783572> if you would like to "
+                        f"ask any questions or get help.")
+        else:
+            content += "\n\nWe don't have any events coming up right now, but stay tuned!"
+
+        await member.send(content=content)
 
     async def on_scheduled_event_create(self, scheduled_event: discord.ScheduledEvent):
         print(f"Sending event '{scheduled_event.name}' to supabase")
